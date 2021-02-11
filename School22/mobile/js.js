563f7247a8b7955f5c8aac87db3b8ffc847c4ada
skrinss <59201155+skrinss@users.noreply.github.com>
2021-02-11 22:44:14 +0300

    Add files via upload

---
+++ School22/mobile/js.js
@@ -0,0 +1,116 @@
+let First = false;
+let Second = false;
+let Third = false;
+let Fourth = false;
+let Five = false;
+let Six = false;
+let Seven = false;
+let Eight = false;
+let Nine = false;
+let Ten = false;
+
+
+
+
+
+function Firsts() {
+	if (First == false) {
+		First = true;
+		$('.DocumentFirst').css('display','block');
+	} else {
+		First = false;
+		$('.DocumentFirst').css('display','none');
+	}
+}
+
+function Seconds() {
+	if (Second == false) {
+		Second = true;
+		$('.DocumentSecond').css('display','block');
+	} else {
+		Second = false;
+		$('.DocumentSecond').css('display','none');
+	}
+}
+
+function Thirds() {
+	if (Third == false) {
+		Third = true;
+		$('.DocumentThird').css('display','block');
+	} else {
+		Third = false;
+		$('.DocumentThird').css('display','none');
+	}
+}
+
+function Furths() {
+	if (Fourth == false) {
+		Fourth = true;
+		$('.DocumentFourths').css('display','block');
+	} else {
+		Fourth = false;
+		$('.DocumentFourths').css('display','none');
+	}
+}
+
+function Fives() {
+	if (Five == false) {
+		Five = true;
+		$('.DocumentFive').css('display','block');
+	} else {
+		Five = false;
+		$('.DocumentFive').css('display','none');
+	}
+}
+
+
+
+function Sixth() {
+	if (Six == false) {
+		Six = true;
+		$('.DocumentSixthss').css('display','block');
+	} else {
+		Six = false;
+		$('.DocumentSixthss').css('display','none');
+	}
+}
+
+function Seventh() {
+	if (Seven == false) {
+		Seven = true;
+		$('.DocumentSeventh').css('display','block');
+	} else {
+		Seven = false;
+		$('.DocumentSeventh').css('display','none');
+	}
+}
+
+function Eights() {
+	if (Eight == false) {
+		Eight = true;
+		$('.DocumentEights').css('display','block');
+	} else {
+		Eight = false;
+		$('.DocumentEights').css('display','none');
+	}
+}
+
+function Nineths() {
+	if (Nine == false) {
+		Nine = true;
+		$('.DocumentNine').css('display','block');
+	} else {
+		Nine = false;
+		$('.DocumentNine').css('display','none');
+	}
+}
+
+function Tenss() {
+	if (Ten == false) {
+		Ten = true;
+		$('.DocumentTens').css('display','block');
+	} else {
+		Ten = false;
+		$('.DocumentTens').css('display','none');
+	}
+}
