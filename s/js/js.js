0a62378cee3e1137aa5ede8676ffbc064749b97a
skrinss <vladik-panshin@mail.ru>
2022-11-10 01:52:32 +0300

    innitial commit

---
+++ s/js/js.js
@@ -0,0 +1,10 @@
+// jQuery(function($) {
+//         $(window).scroll(function(){
+//             if($(this).scrollTop()>470){
+//                 $('#section-hotbar').addClass('fixed');
+//             }
+//             else if ($(this).scrollTop()<470){
+//                 $('#section-hotbar').removeClass('fixed');
+//             }
+//         });
+//     });
